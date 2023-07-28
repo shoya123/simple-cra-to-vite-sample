16ad65126441428ba3885a24d20d6ef3c3223038
shoya123 <>
2023-07-28 13:33:23 +0900

    feat: remove unneeded packages

--- jest.config.js
+++ jest.config.js
@@ -8,7 +8,6 @@ const esmPackages = [
 module.exports = {
   roots: ["<rootDir>/src"],
   collectCoverageFrom: ["src/**/*.{js,jsx,ts,tsx}", "!src/**/*.d.ts"],
-  setupFiles: ["react-app-polyfill/jsdom"],
   setupFilesAfterEnv: ["<rootDir>/src/setupTests.ts"],
   testMatch: [
     "<rootDir>/src/**/__tests__/**/*.{js,jsx,ts,tsx}",
